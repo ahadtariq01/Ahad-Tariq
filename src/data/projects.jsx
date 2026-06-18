592970763cb184bd29d9bb0a67a7d117a59a66ed
Ahad Tariq <ahadtariq616@gmail.com>
2026-06-18 15:57:57 +0500

    Update projects.jsx

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -24,7 +24,7 @@ export const projects = [
     heading: "CliqueZorg | Medical Knowledge",
     discr: "Medical Articles",
     tags: [
-      { name: "Mongo", icon: <SiMongodb color="#47A248" /> },
+     { name: "Elementor", icon: "" },
     ],
   },
   {
@@ -34,7 +34,7 @@ export const projects = [
     heading: "Horizon Surgical Center",
     discr: "Medical Center",
     tags: [
-      { name: "Firebase", icon: <SiFirebase color="#FFCA28" /> },
+     { name: "Elementor", icon: "" },
     ],
   },
   {
@@ -44,7 +44,7 @@ export const projects = [
     heading: "Armour Homes",
     discr: "Custom Home Builders",
     tags: [
-      { name: "TypeScript", icon: <SiTypescript color="#3178C6" /> },
+      { name: "Elementor", icon: "" },
     ],
   },
   // Placeholders for future projects
@@ -55,7 +55,7 @@ export const projects = [
     heading: "Saltec Group",
     discr: "Cutting-Edge AV Solutions",
     tags: [
-      { name: "React", icon: <FaReact color="#61DAFB" /> },
+      { name: "Elementor", icon: "" },
     ],
   },
  {
@@ -65,7 +65,7 @@ export const projects = [
     heading: "Tradex Desk",
     discr: "Performance-Driven Trading",
     tags: [
-      { name: "React", icon: <FaReact color="#61DAFB" /> },
+     { name: "Elementor", icon: "" },
     ],
   }, // <--- Yeh comma lagana boht zaroori hai naye project se pehle!
   {
@@ -75,7 +75,7 @@ export const projects = [
     heading: "Tidwell",
     discr: "Bookkeeping Services",
     tags: [
-      { name: "React", icon: <FaReact color="#61DAFB" /> },
+     { name: "Elementor", icon: "" },
     ],
   } // <--- Aakhri project ke baad comma nahi aayega
 ]; // <--- Yeh square bracket aur semicolon poori list ko close karta hai
